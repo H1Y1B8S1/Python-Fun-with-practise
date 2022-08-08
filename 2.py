54fc5b3e017e666016b9ded7565981021948fbc1
Siddharthsinh Dodiya <h1y1b8s1@gmail.com>
2022-08-07 22:44:43 -0700

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,26 @@
+dic = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+lis = ['I', 'V', 'X', 'L', 'C', 'D', 'M']
+
+
+s='MCMXCIV'
+IN = 0
+Str = list(s)
+Str.reverse()
+i = 0
+
+while i < (len(Str)):
+    IN +=dic[Str[i]]
+    # print(f'{i} {IN}')
+    if i<(len(Str)-1):
+        while lis.index(Str[i+1]) < lis.index(Str[i]):
+            IN -=dic[Str[i+1]]
+            i+=1
+            # print(f'{i} {IN}')
+            if i >= (len(Str)-1):
+                break
+
+
+    i+=1
+
+
+print(IN)
